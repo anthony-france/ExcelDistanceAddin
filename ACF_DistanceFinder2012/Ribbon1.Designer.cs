c565729df29e91d18a6f34f89e9902dff34737fa
Anthony France <afrance@pointimaging.com>
2012-08-02 09:55:18 -0500

    Version 1

---
+++ ACF_DistanceFinder2012/Ribbon1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ACF_DistanceFinder2012
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculateDistances = this.Factory.CreateRibbonButton();
+            // 
+            // btnCalculateDistances
+            // 
+            this.btnCalculateDistances.Image = global::ACF_DistanceFinder2012.Properties.Resources.bmeasureoff;
+            this.btnCalculateDistances.Label = "Calculate";
+            this.btnCalculateDistances.Name = "btnCalculateDistances";
+            this.btnCalculateDistances.ShowImage = true;
+            this.btnCalculateDistances.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnCalculateDistances_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            // 
+            // Ribbon1.OfficeMenu
+            // 
+            this.OfficeMenu.Items.Add(this.btnCalculateDistances);
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnCalculateDistances;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ ACF_DistanceFinder2012/Ribbon1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Ribbon;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Collections;
+
+namespace ACF_DistanceFinder2012
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnCalculateDistances_Click(object sender, RibbonControlEventArgs e)
+        {
+
+            // Setup ranges.  
+            Range rngSource;
+            Range rngDest;
+            Range rngOutput;
+            // Probably a better idea to instead have the user setup named ranges.
+
+            // Prompt the user to select each of our required points.
+            // If it fails tell the user and return method.
+            try
+            {
+                rngSource = Globals.ThisAddIn.Application.InputBox("Source Range", "Source", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, 8);
+                rngDest = Globals.ThisAddIn.Application.InputBox("Destination Columns", "Destinations", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, 8);
+                rngOutput = Globals.ThisAddIn.Application.InputBox("Output Starting Cell", "Output", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, 8);
+            }
+            catch
+            {
+                MessageBox.Show("Error getting input.");
+                return;
+            }
+
+            // counters for filling in the output cells.
+            int rowOffset = 1;
+            int colOffset = 0;
+
+            // List of the destination headers.
+           List<string> Destinations = new List<string>();
+
+            // Add the colums to the Destination list to use later.
+            // If it fails we need to let the user know and stop.
+           try
+           {
+               foreach (Range rowDest in rngDest.Columns)
+               {
+                   Destinations.Add(System.Convert.ToString(rowDest.Value2));
+               }
+           }
+           catch
+           {
+              MessageBox.Show("Unable to get destinations.");
+              return;
+           }
+
+            // Loop through the list of source locations.
+            foreach (Range sourceCell in rngSource.Rows)
+            {
+                // declare in the scope for using later.
+                List<double> distance = new List<double>();
+                string json;
+
+                // Setup the request url.  
+                //Better to use the uribuilder but speed isn't much of a concern.
+                string url = "http://maps.googleapis.com/maps/api/distancematrix/json?sensor=false";
+                url += "&origins=" + System.Convert.ToString(sourceCell.Value2);
+                url += "&destinations=";
+
+                foreach (string Destination in Destinations)
+                {
+                    url += Destination.ToString() + "|";
+                }
+
+                // Create the webrequest.
+                // Try the next row if it fails; continue on the loop.
+                try
+                {
+                    WebRequest request = WebRequest.Create(url);
+                    WebResponse response = request.GetResponse();
+
+                    Stream dataStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(dataStream);
+                    json = reader.ReadToEnd();
+                }
+                catch {
+                    //MessageBox.Show("Error fetching data.");
+                    continue;
+                }
+
+                // Create list of returned values.
+                // Continue on if fails.
+                try{
+                    JObject jObject = JObject.Parse(json);
+                    
+                    foreach (JObject rows in jObject["rows"].Children())
+                    {
+                        foreach (JObject elements in rows["elements"].Children())
+                        {
+                            distance.Add((double)elements["distance"]["value"] * 0.00062137119);
+                         }
+                    }
+                }
+                catch
+                {
+                    //MessageBox.Show("Error parsing data.");
+                    continue;
+                }
+
+                // Place the values.
+                // If failed continue on.
+                try
+                {
+                    colOffset = 0;
+                    foreach (double d in distance)
+                    {
+                        rngOutput[rowOffset, colOffset + 1].Value = distance[colOffset];
+                        rngOutput[rowOffset, colOffset + 1].Show();
+                        
+                        colOffset++;
+                    }
+                }
+                catch
+                {
+                    //MessageBox.Show("Problem setting data in output.");
+                    continue;
+                }
+                rowOffset++;
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+
+    }
+}
